b8f9624977c08c152c4705761bfa211fb8958e17
dholandac <daniel.holanda.campos@gmail.com>
2025-10-01 17:13:41 -0300

    ajustes de seguranca

--- investichat/settings.py
+++ investichat/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx2hqpn+abwwyzh75hzo8@@lb5^rag^gv@x&00k6tx!l*lvm(d'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
