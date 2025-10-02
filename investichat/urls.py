e1791570ef021aff08af307e6432b93d014ea0af
dholandac <daniel.holanda.campos@gmail.com>
2025-10-02 15:04:24 -0300

    adicionando redirecionamento

--- investichat/urls.py
+++ investichat/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/chatbot/', permanent=False)),
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('chatbot/', include('chatbot.urls'))
