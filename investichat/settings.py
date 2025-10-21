88b123845969d8bba34723a458980299c0fa7f1e
Daniel <161149775+dholandac@users.noreply.github.com>
2025-10-21 15:37:32 -0300

    alteracao

--- investichat/settings.py
+++ investichat/settings.py
@@ -32,7 +32,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'investichat.up.railway.app',
